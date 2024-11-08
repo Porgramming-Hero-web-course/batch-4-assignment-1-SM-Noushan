89f9cce9db877f78f0c7e66a16daa888fc5695b0
SMNoushan <noushan.md@gmail.com>
2024-11-08 16:13:00 +0600

    update: problem-5

--- problem-5.ts
+++ problem-5.ts
@@ -1,6 +1,6 @@
 {
-  const getProperty = <T, K extends keyof T>(obj: T, key: K) => obj[key];
+  const getProperty = <T, K extends keyof T>(obj: T, key: K): T[K] => obj[key];
 
   //   const person = { name: "Alice", age: 30 };
-  //   console.log(getProperty(person, "age"));
+  //   console.log(getProperty(person, "name"));
 }
