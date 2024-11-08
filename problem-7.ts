95244ffb94245c8bf6bcefed5466ca4136a33816
SMNoushan <noushan.md@gmail.com>
2024-11-08 16:16:52 +0600

    problem-7

---
+++ problem-7.ts
@@ -0,0 +1,15 @@
+{
+  class Car {
+    constructor(
+      public make: string,
+      public model: string,
+      public year: number
+    ) {}
+    getCarAge() {
+      return new Date().getFullYear() - this.year;
+    }
+  }
+
+  //   const car = new Car("Honda", "Civic", 2018);
+  //   console.log(car.getCarAge());
+}
