d10a528b72301e25fef55ad30872a1f37d4c3b9a
SMNoushan <noushan.md@gmail.com>
2024-11-08 14:39:22 +0600

    problem-1

---
+++ problem-1.ts
@@ -0,0 +1,8 @@
+{
+  const sumArray = (arr: number[]): number =>
+    arr.reduce(
+      (accumulator: number, current: number) => accumulator + current,
+      0
+    );
+  //   console.log(sumArray([1, 2, 3, 4, 5]));
+}
